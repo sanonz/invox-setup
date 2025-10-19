d2d5b6694ef0b2c4e348e3f02659c37234f37ae9
Sanonz <sanonz@126.com>
2025-10-19 11:58:26 +0800

    fix: 修复卸载读取注册表错误问题

--- Uninstaller/UninstallerWnd.cpp
+++ Uninstaller/UninstallerWnd.cpp
@@ -28,7 +28,7 @@ CUninstallerWnd::CUninstallerWnd()
     , m_bUninstalling(false)
 {
     // 从注册表读取安装路径
-    std::wstring regPath = std::wstring(REG_UNINSTALL_PATH) + APP_NAME;
+    std::wstring regPath = std::wstring(REG_UNINSTALL_PATH) + APP_REGISTRY_KEYS;
     
     HKEY hKey = NULL;
     if (RegOpenKeyEx(HKEY_LOCAL_MACHINE, regPath.c_str(), 0, KEY_READ, &hKey) == ERROR_SUCCESS ||
